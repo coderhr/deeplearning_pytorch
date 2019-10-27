22de5768ea55d1fe8294ca2b03a4d540402be09f
huangrui2 <huangrui2@sensetime.com>
2019-10-27 13:21:29 +0800

    3

---
+++ 3/excise1.py
@@ -0,0 +1,16 @@
+import torch
+from time import time
+
+a = torch.ones(1000)
+b = torch.ones(1000)
+
+start = time()
+c = torch.zeros(1000)
+for i in range(1000):
+    c[i] = a[i] + b[i]
+
+print(time() - start)
+
+start = time()
+d = a + b
+print(time() - start)
